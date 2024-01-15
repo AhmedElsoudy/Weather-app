4969eb33e163b49978c1e193cef898e544df4d5c
AhmedElsoudy <ajmd9563@gmail.com>
2024-01-15 19:15:26 +0000

    second commit

--- logic/logic.js
+++ logic/logic.js
@@ -23,17 +23,16 @@ let nextDayText = document.getElementsByClassName("next-day-text")
 
 //  ================ Search Input ==================
 
-let searchInput = document.getElementById("search")
+let searchInput = document.getElementById("btn")
 
 //  fetch API
 
 async function getData(cityName){
     let weatherResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=756295618e574c8c906162634241101&q=${cityName}&days=3`)
     let weatherData = await weatherResponse.json()
-    console.log(weatherData);
     return weatherData
 }
-getData()
+// getData()
 
 function displayTodayData(data){
     let todayDate = new Date()
@@ -56,25 +55,29 @@ function displayNextDayData(data){
     for(let i =0; i<2; i++){
         let nextDate = new Date(Data[i+1].date)
         nextDay[i].innerHTML = nextDate.toLocaleDateString("en-US",{weekday:"long"})
-        nextDayMaxTemp[i].innerHTML = Data[i+1].day.maxtemp_c + "&deg;c"
-        nextDayMinTemp[i].innerHTML = Data[i+1].day.mintemp_c  + "&deg;c"
+        nextDayMaxTemp[i].innerHTML = Data[i+1].day.maxtemp_c
+        nextDayMinTemp[i].innerHTML = Data[i+1].day.mintemp_c 
         nextDayImg[i].setAttribute("src",Data[i+1].day.condition.icon)
         nextDayText[i].innerHTML = Data[i+1].day.condition.text
     }
 }
 
-async function runProject(city="cairo"){
-    let weatherData = await getData(city)
+async function runProject(cityName="cairo"){
+    let weatherData = await getData(cityName)
+    // console.log(weatherData)
     if(!weatherData.error){
         displayTodayData(weatherData)
         displayNextDayData(weatherData)
     }
-    
 }
-
 runProject()
 
-searchInput.addEventListener("input",function(){
+
+searchInput.addEventListener("input", function(){
+    console.log(searchInput.value)
     runProject(searchInput.value)
 })
+
+
+
 
